76310a97d1ea43659724b2b823b73268f7b89a45
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-02-01 15:16:25 -0800

    working on main_menu.h

--- main_menu.h
+++ main_menu.h
@@ -3,10 +3,30 @@
 
 #include <SDL2/SDL.h> // THE SDL LIBRARY
 
-class MainMenu{
+class MainMenu
+{
 public:
     MainMenu();
-    void print();
+    void print(); // test function
+
+    /* start up SDL and create window */
+    bool initalize();
+
+    /* loads the media */
+    bool loadMedia();
+
+    /* Frees media and shuts down SDL */
+    void close();
+
+private:
+    /* the window to render to */
+    SDL_Window* mainmenu_window = NULL;
+
+    /* The surface containd by the window */
+    SDL_Surface* mainmenu_surface = NULL;
+
+    
+
 };
 
 #endif
