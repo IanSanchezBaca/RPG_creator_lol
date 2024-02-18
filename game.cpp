39bcb03df4b2adbf11f632b973e4faad5455c1c0
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-02-18 07:11:24 -0800

    added stuff to update() that moves the image around in the screen

--- game.cpp
+++ game.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-SDL_Texture* playerTex;
 
 // constructor
 Game::Game(){}
@@ -29,7 +28,7 @@ void Game::initialize(char* title, int xpos, int ypos, int width, int height){
         
         renderer = SDL_CreateRenderer(window, -1, 0);
         if(renderer){
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             cout << "Renderer created successfully!\n";
         }
         else{
@@ -67,7 +66,10 @@ void Game::EventHandler(){
 }
 
 void Game::update(){
-    // test_counter++;
+    test_counter++;
+    destR.h = 64;
+    destR.w = 64;
+    destR.x = test_counter;
     // cout << test_counter << endl;
 }
 
@@ -75,7 +77,7 @@ void Game::render(){
     SDL_RenderClear(renderer);
     // we would add stuff to render here
 
-    SDL_RenderCopy(renderer, playerTex, NULL, NULL);
+    SDL_RenderCopy(renderer, playerTex, NULL, &destR);
 
     SDL_RenderPresent(renderer);
 }
@@ -85,6 +87,7 @@ void Game::close(){
     SDL_DestroyRenderer(renderer);
     SDL_Quit();
     cout << "Game cleaned!\n";
+    cout << "Counter: " << test_counter << endl;
 }
 
 bool Game::running(){
