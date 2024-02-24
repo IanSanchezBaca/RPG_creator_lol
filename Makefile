84fffd1c2e1fd0a8225adfabd71744138d77afae
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-02-24 12:49:20 -0800

    created a simple texture manager

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ CFLAGS = -Wall -Wextra -std=c++11
 LDFLAGS = -lSDL2 -lSDL2_image
 
 # Source files
-SRCS = main.cpp game.cpp
+SRCS = main.cpp game.cpp texture_manager.cpp
 
 # Object files
 OBJS = $(SRCS:.cpp=.o)
---
+++ texture_manager.h
@@ -0,0 +1,14 @@
+// #ifndef TEXTURE_MANAGER_H
+// #define TEXTURE_MANAGER_H
+
+#include "game.h"
+
+class TextureManager
+{
+public:
+    static SDL_Texture *LoadTexture(const char *fileName, SDL_Renderer *ren);
+
+private:
+};
+
+// #endif
