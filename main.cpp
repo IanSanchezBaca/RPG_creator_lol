6a1d1127141a467953be54d79944b6239b73a011
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-02-24 12:48:42 -0800

    added fps lmao

--- main.cpp
+++ main.cpp
@@ -12,8 +12,12 @@ Game *game = NULL;
 
 int main(int argc, char *args[])
 {
-    // const int FPS = 60;
+    const int FPS = 60;
     // will use this to limit fps? lol
+
+    const int frameDelay = 1000 / FPS;
+    Uint32 frameStart;
+    int frameTime; 
 
     game = new Game();
 
@@ -25,9 +29,17 @@ int main(int argc, char *args[])
 
     while (game->running())
     {
+        frameStart = SDL_GetTicks();
         game->EventHandler();
         game->update();
         game->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+    if(frameDelay > frameTime){
+        SDL_Delay(frameDelay - frameTime);
+    }
+
     }
 
     game->close();
