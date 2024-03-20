cdb235cf08355964c43b9a3ebfb289fbcce87973
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-03-20 15:05:24 -0700

    created TileComponent.h having trouble currently

---
+++ ECS/TileComponent.h
@@ -0,0 +1,57 @@
+#ifndef TILECOMPONENT_H
+#define TILECOMPONENT_H
+#include "ECS.h"
+#include "TransformComponent.h"
+#include "SpriteComponents.h"
+#include "SDL2/SDL.h"
+
+class TileComponent : public Component
+{
+private:
+    /* data */
+public:
+    TransformComponent *transform;
+    SpriteComponent *sprite;
+
+    SDL_Rect tileRect;
+    int tileID;
+    char *path;
+
+    TileComponent() = default;
+
+    TileComponent(int x, int y, int w, int h, int id)
+    {
+        tileRect.x = x;
+        tileRect.y = y;
+        tileRect.w = w;
+        tileRect.h = h;
+        tileID = id;
+
+        switch (tileID)
+        {
+        case 0:
+            path = "../media/background/water.png";
+            break;
+        case 1:
+            path = "../media/background/dirt.png";
+            break;
+        case 2:
+            path = "../media/background/grass.png";
+            break;
+        default:
+
+            break;
+        }
+    }
+
+    void init() override
+    {
+        entity->addComponent<TransformComponent>((float)tileRect.x, (float)tileRect.y, (float)tileRect.w, (float)tileRect.h, 1);
+        transform = &entity->getComponent<TransformComponent>();
+
+        entity->addComponent<SpriteComponent>(path);
+        sprite = &entity->getComponent<SpriteComponent>();
+    }
+};
+
+#endif
