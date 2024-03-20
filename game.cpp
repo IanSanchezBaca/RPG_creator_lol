23a350cca69efe1cfebcfc64dcc36b6f39aa3ce5
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-03-20 15:04:31 -0700

    Added implamentation of ColliderComponent

--- game.cpp
+++ game.cpp
@@ -14,8 +14,14 @@ SDL_Renderer *Game::renderer = NULL;
 
 SDL_Event Game::event;
 
+std::vector<ColliderComponent *> Game::colliders;
+
 auto &player(manager.addEntity());
 auto &wall(manager.addEntity());
+
+auto &tile0(manager.addEntity());
+auto &tile1(manager.addEntity());
+auto &tile2(manager.addEntity());
 
 // constructor
 Game::Game() {}
@@ -64,6 +70,13 @@ void Game::initialize(char *title, int xpos, int ypos, int width, int height)
     map = new Map();
 
     // ecs implementation
+
+    tile0.addComponent<TileComponent>(200, 200, 32, 32, 0);
+    tile1.addComponent<TileComponent>(250, 250, 32, 32, 1);
+    tile1.addComponent<ColliderComponent>("dirt");
+    tile2.addComponent<TileComponent>(150, 150, 32, 32, 2);
+    tile2.addComponent<ColliderComponent>("grass");
+
     player.addComponent<TransformComponent>(2);
     player.addComponent<SpriteComponent>("media/characters/Rito.png");
     player.addComponent<KeyboardController>();
