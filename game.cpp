f4e90dcadd83dc1750182f0e1767e99b4e24185a
Ian Sanchez Baca <iansanchez066@gmail.com>
2024-04-24 16:47:41 -0700

    working on event handler

---
+++ game.cpp
@@ -0,0 +1,95 @@
+#include "game.h"
+
+SDL_Renderer *Game::renderer = NULL;
+SDL_Event Game::event;
+
+Game::Game() {}
+
+void Game::initialize(string title, int xpos, int ypos, int width, int height)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    {
+        // string t = title;
+        cout << "SDL Initialized correctly.\n";
+
+        window = SDL_CreateWindow(title.c_str(), xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+
+        if (window)
+        {
+            cout << "Window created successfully!\n";
+        }
+        else
+        {
+            cout << "Error: " << SDL_GetError() << endl;
+            exit(-1);
+        }
+
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        if (renderer)
+        {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            cout << "Renderer created successfully!\n";
+        }
+        else
+        {
+            cout << "ERROR: " << SDL_GetError() << endl;
+            exit(-1);
+        }
+    }
+    else
+    {
+        cout << "ERROR: " << SDL_GetError() << endl;
+        exit(-1);
+    }
+    isRunning = true;
+} // initialize
+
+void Game::EventHandler()
+{
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT)
+    {
+        /* Pretty much if alt-f4 or the 'x' is pressed */
+        isRunning = false;
+        return;
+    }
+    switch (event.type)
+    {
+    case SDL_MOUSEBUTTONDOWN:
+        if (event.button.button == SDL_BUTTON_LEFT)
+        {
+            // Get the mouse coordinates
+            x = event.button.x;
+            y = event.button.y;
+            cout << "mouse was clicked at pos: " << x << " " << y << "\n";
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+void Game::update()
+{
+    // cout << "Updating...\n";
+}
+
+void Game::render()
+{
+    // cout << "Rendering...\n";
+}
+
+void Game::close()
+{
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    cout << "Game cleaned!\n";
+    // cout << "Counter: " << test_counter << endl;
+} // close
+
+bool Game::running()
+{
+    return isRunning;
+} // running
---
+++ game.h
@@ -0,0 +1,51 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include <vector>
+// #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+// class ColliderComponent;
+
+class Game
+{
+public:
+    Game();
+
+    void test_print();
+
+    void initialize(string title,
+                    int xpos,
+                    int ypos,
+                    int width,
+                    int height);
+
+    void EventHandler();
+
+    void update();
+
+    void render();
+
+    void close();
+
+    bool running();
+
+    static SDL_Renderer *renderer;
+
+    static SDL_Event event;
+
+    void round(int, int);
+
+private:
+    int x, y; // should be the position that the mouse clicked
+
+    bool isRunning;
+
+    SDL_Window *window = NULL;
+};
+
+#endif
